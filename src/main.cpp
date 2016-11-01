ddb75bbe263442255aadda5cfd194ca70aa64d47
Pranshu Gupta <pranshug258@outlook.com>
2016-11-02 01:38:29 +0530

    defined the workflow of the program

--- src/main.cpp
+++ src/main.cpp
@@ -2,3 +2,19 @@
 
 using namespace std;
 
+int main() {
+    // Create the Comparison Model
+
+    // Create a Dataset
+    int N, D;
+    cout << "CREATING TEST DATASET\n" << "enter the number of samples: "; cin >> N;
+    cout << "enter the number of dimensions: "; cin >> D;
+    
+    // Sort the Dataset on each dimension
+
+    // Find the Skyline Set
+
+    // Find the Representative Skyline Set
+
+    return 0;
+}
